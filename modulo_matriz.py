9a1caf1fe36c142747003b938491e1d9ce6af065
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-22 11:39:21 -0300

    correcao

---
+++ modulo_matriz.py
@@ -0,0 +1,30 @@
+from random import randint
+
+def preenche_matriz(lin: int, col: int) -> list:
+    '''Prenche a matriz com entrada do usuario'''
+    matriz = []
+    for i in range(lin):
+        linha = []
+        for j in range(col):
+            n = int(input('Número: '))
+            linha.append(n)
+        matriz.append(linha)
+    return matriz
+
+def preencher_matriz_aleatoria(lin: int, col: int) -> list:
+    #preencher a matriz com numeros aleatorios 
+    matriz = []
+    for i in range(lin):      # linha
+        linha = []      
+        for j in range(col):      # coluna
+            n = randint(1, 50)
+            linha.append(n)
+        matriz.append(linha)
+    return matriz
+
+def exibe_matriz(matriz) -> None:
+    '''imprime a matriz'''
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            print(matriz[i][j], end='\t')
+        print()
